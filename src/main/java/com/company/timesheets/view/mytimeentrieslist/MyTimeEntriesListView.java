29d00f70e0a88b705afff3e83e16be30c15487b1
vashenkoda <d.vaschenko@haulmont.net>
2025-04-14 21:31:35 +0300

    Part two

--- src/main/java/com/company/timesheets/view/mytimeentrieslist/MyTimeEntriesListView.java
+++ src/main/java/com/company/timesheets/view/mytimeentrieslist/MyTimeEntriesListView.java
@@ -1,14 +1,39 @@
 package com.company.timesheets.view.mytimeentrieslist;
 
 
+import com.company.timesheets.app.TimeEntrySupport;
+import com.company.timesheets.entity.TimeEntry;
 import com.company.timesheets.view.main.MainView;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.StandardView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.DialogWindows;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "my-time-entries", layout = MainView.class)
 @ViewController(id = "ts_TimeEntry.my")
 @ViewDescriptor(path = "my-time-entries-list-view.xml")
 public class MyTimeEntriesListView extends StandardView {
+
+    @ViewComponent
+    private DataGrid<TimeEntry> timeEntriesDataGrid;
+    @Autowired
+    private TimeEntrySupport timeEntrySupport;
+    @Autowired
+    private DialogWindows dialogWindows;
+
+    @Subscribe("timeEntriesDataGrid.copy")
+    public void onTimeEntriesDataGridCopy(final ActionPerformedEvent event) {
+        TimeEntry selected = timeEntriesDataGrid.getSingleSelectedItem();
+        if (selected == null) {
+            return;
+        }
+
+        TimeEntry copied = timeEntrySupport.copy(selected);
+
+        dialogWindows.detail(timeEntriesDataGrid)
+                .newEntity(copied)
+                .open();
+    }
 }
--- src/main/java/com/company/timesheets/view/project/ProjectDetailView.java
+++ src/main/java/com/company/timesheets/view/project/ProjectDetailView.java
@@ -3,6 +3,7 @@ package com.company.timesheets.view.project;
 import com.company.timesheets.entity.Project;
 import com.company.timesheets.view.main.MainView;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
 import io.jmix.flowui.view.*;
 
 @Route(value = "projects/:id", layout = MainView.class)
@@ -11,4 +12,5 @@ import io.jmix.flowui.view.*;
 @EditedEntityContainer("projectDc")
 @DialogMode(width = "64em")
 public class ProjectDetailView extends StandardDetailView<Project> {
+
 }
